83c08211b2bc7c117bcc2c7142bc2406af82787d
MenaceHecker <thedynamic2508@gmail.com>
2026-01-25 08:41:10 -0500

    Updated App Nav graph with composables

--- app/src/main/java/com/example/signalops/ui/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/signalops/ui/navigation/AppNavGraph.kt
@@ -1,4 +1,19 @@
 package com.example.signalops.ui.navigation
 
-class AppNavGraph {
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavGraph() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "login"
+    ) {
+        composable("login") { LoginScreen(navController) }
+        composable("dashboard") { DashboardScreen() }
+    }
 }
