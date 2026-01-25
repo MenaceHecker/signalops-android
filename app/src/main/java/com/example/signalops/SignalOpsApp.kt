57f63f0bb7133f226c841212b5b0f6f194358ce5
MenaceHecker <thedynamic2508@gmail.com>
2026-01-25 17:36:51 -0500

    Fixed duplicate file error

--- app/src/main/java/com/example/signalops/SignalOpsApp.kt
+++ app/src/main/java/com/example/signalops/SignalOpsApp.kt
@@ -2,6 +2,7 @@ package com.example.signalops
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import com.example.signalops.ui.navigation.AppNavGraph
 
 @Composable
 fun SignalOpsApp() {
