aeddc767349e0b25af7f3b8faf638026c94834e6
MenaceHecker <thedynamic2508@gmail.com>
2026-01-27 15:54:41 -0500

    Initialized navigateBack and signUp route variables

--- app/src/main/java/com/example/signalops/app/auth/SignupScreen.kt
+++ app/src/main/java/com/example/signalops/app/auth/SignupScreen.kt
@@ -1,4 +1,18 @@
 package com.example.signalops.app.auth
 
-class SignupScreen {
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SignupScreen(
+    onNavigateBack: () -> Unit,
+    onSignupSuccess: () -> Unit,
+)
+{
+
 }
