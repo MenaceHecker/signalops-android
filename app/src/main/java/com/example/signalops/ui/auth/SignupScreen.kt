cfc9f9068e7d3866775c24300a7ea7bd4625d469
MenaceHecker <thedynamic2508@gmail.com>
2026-01-26 22:36:35 -0500

    Added necessary imports

--- app/src/main/java/com/example/signalops/ui/auth/SignupScreen.kt
+++ app/src/main/java/com/example/signalops/ui/auth/SignupScreen.kt
@@ -1,4 +1,13 @@
 package com.example.signalops.ui.auth
 
-class SignupScreen {
-}
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Mail
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.*
+import androidx.compose.ui.unit.dp
