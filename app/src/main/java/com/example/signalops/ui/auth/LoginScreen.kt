60501b19179809d352e8de8b0c1c4576f8c79cb2
MenaceHecker <thedynamic2508@gmail.com>
2026-01-26 22:28:23 -0500

    Updated the necessary imports

--- app/src/main/java/com/example/signalops/ui/auth/LoginScreen.kt
+++ app/src/main/java/com/example/signalops/ui/auth/LoginScreen.kt
@@ -1,9 +1,13 @@
 package com.example.signalops.ui.auth
 
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
-
-@Composable
-fun LoginScreen(navController: NavController) {
-    ("Login")
-}
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Mail
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.*
+import androidx.compose.ui.unit.dp
