942da7b91c5896cff31fd8265113b9ae52fe2abe
MenaceHecker <thedynamic2508@gmail.com>
2026-01-28 14:37:45 -0500

    Updated mainactivity with new ic_launcher logo

--- app/src/main/java/com/example/signalops/MainActivity.kt
+++ app/src/main/java/com/example/signalops/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.navigation.compose.rememberNavController
-import com.example.signalops.ui.auth.AuthNavGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,18 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val navController = rememberNavController()
-
-            MaterialTheme {
-                Surface {
-                    AuthNavGraph(
-                        navController = navController,
-                        onAuthSuccess = {
-                            // Will add navigation to dashboard later
-                        }
-                    )
-                }
-            }
+            SignalOpsApp()
         }
     }
 }
