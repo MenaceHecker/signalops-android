43456834c8665b14342c656a3270122a76d68d25
MenaceHecker <thedynamic2508@gmail.com>
2026-02-03 15:00:41 -0500

    email, password returned, not name

--- app/src/main/java/com/example/signalops/data/remote/dto/AuthDtos.kt
+++ app/src/main/java/com/example/signalops/data/remote/dto/AuthDtos.kt
@@ -1,4 +1,15 @@
 package com.example.signalops.data.remote.dto
 
-class AuthDtos {
-}
+data class RegisterRequest(
+    val email: String,
+    val password: String
+)
+
+data class LoginRequest(
+    val email: String,
+    val password: String
+)
+
+data class AuthResponse(
+    val token: String
+)
