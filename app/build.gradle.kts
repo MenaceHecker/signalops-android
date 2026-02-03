983e0f9f5bfee50574eede5f86c8bdc8916df5a6
MenaceHecker <thedynamic2508@gmail.com>
2026-02-03 14:55:42 -0500

    Updated build gradle with retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,4 +80,9 @@ dependencies {
     // For Splashscreen
     implementation("androidx.core:core-splashscreen:1.0.1")
 
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
