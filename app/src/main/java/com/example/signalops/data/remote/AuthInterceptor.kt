7385dd09faa74939e6a073f426943f4214cc5dab
MenaceHecker <thedynamic2508@gmail.com>
2026-02-07 18:43:39 -0500

    Added jwt interceptor in android

--- app/src/main/java/com/example/signalops/data/remote/AuthInterceptor.kt
+++ app/src/main/java/com/example/signalops/data/remote/AuthInterceptor.kt
@@ -6,19 +6,20 @@ import okhttp3.Response
 
 class AuthInterceptor(
     private val tokenStore: TokenStore
-) : Interceptor {
+) : okhttp3.Interceptor {
 
-    override fun intercept(chain: Interceptor.Chain): Response {
-        val token = tokenStore.get()
+    override fun intercept(chain: okhttp3.Interceptor.Chain): okhttp3.Response {
         val req = chain.request()
+        val path = req.url.encodedPath
+        val isAuth = path.startsWith("/api/auth/")
+        val token = tokenStore.get()
 
-        val authedReq =
-            if (!token.isNullOrBlank()) {
-                req.newBuilder()
-                    .addHeader("Authorization", "Bearer $token")
-                    .build()
-            } else req
+        val newReq = if (!isAuth && !token.isNullOrBlank()) {
+            req.newBuilder()
+                .addHeader("Authorization", "Bearer $token")
+                .build()
+        } else req
 
-        return chain.proceed(authedReq)
+        return chain.proceed(newReq)
     }
 }
