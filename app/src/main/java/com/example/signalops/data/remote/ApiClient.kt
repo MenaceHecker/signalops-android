a05ebd1491150ab12a45b8275b6774a8ba1e7d42
MenaceHecker <thedynamic2508@gmail.com>
2026-02-09 14:52:30 -0500

    Updated base url

--- app/src/main/java/com/example/signalops/data/remote/ApiClient.kt
+++ app/src/main/java/com/example/signalops/data/remote/ApiClient.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://10.0.2.2:8080"
+    private const val BASE_URL = "http://10.0.2.2:8080/"
 
     fun create(tokenStore: TokenStore): AuthApi {
         val logger = HttpLoggingInterceptor().apply {
