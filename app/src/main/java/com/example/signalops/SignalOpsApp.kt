a5763db1d9c456eabecd2a511bb1f30ae1994201
MenaceHecker <thedynamic2508@gmail.com>
2026-02-12 11:15:33 -0500

    Updated app to use new AppNavGraph

--- app/src/main/java/com/example/signalops/SignalOpsApp.kt
+++ app/src/main/java/com/example/signalops/SignalOpsApp.kt
@@ -3,15 +3,10 @@ package com.example.signalops
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
 import com.example.signalops.ui.auth.AuthNavGraph
+import com.example.signalops.ui.navigation.AppNavGraph
 
 @Composable
 fun SignalOpsApp() {
     val navController = rememberNavController()
-
-    AuthNavGraph(
-        navController = navController,
-        onAuthSuccess = {
-            // TODO later: navigate to dashboard/home graph
-        }
-    )
+    AppNavGraph(navController)
 }
