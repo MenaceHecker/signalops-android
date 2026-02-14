c30d08203b51c887cb65a887319591bf4c7b1fe7
MenaceHecker <thedynamic2508@gmail.com>
2026-02-14 16:14:08 -0500

    Cleaned code, removed AuthNavGraph

--- app/src/main/java/com/example/signalops/SignalOpsApp.kt
+++ app/src/main/java/com/example/signalops/SignalOpsApp.kt
@@ -2,15 +2,10 @@ package com.example.signalops
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
-import com.example.signalops.ui.auth.AuthNavGraph
+import com.example.signalops.ui.navigation.AppNavGraph
 
 @Composable
 fun SignalOpsApp() {
     val navController = rememberNavController()
-    AuthNavGraph(
-        navController = navController,
-        onAuthSuccess = {
-            // To add navigation to dashboard/home
-        }
-    )
+    AppNavGraph(navController)
 }
