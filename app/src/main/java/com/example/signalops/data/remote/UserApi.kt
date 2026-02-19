2ba5d1d3a92eff4d1e19bf52ba0cae8dbf7fbd8e
MenaceHecker <thedynamic2508@gmail.com>
2026-02-19 12:07:29 -0500

    Added api/me endpoint in a get method

--- app/src/main/java/com/example/signalops/data/remote/UserApi.kt
+++ app/src/main/java/com/example/signalops/data/remote/UserApi.kt
@@ -1,4 +1,9 @@
 package com.example.signalops.data.remote
 
-class UserApi {
+import com.example.signalops.data.remote.dto.UserProfileResponse
+import retrofit2.http.GET
+
+interface UserApi {
+    @GET("/api/me")
+    suspend fun me(): UserProfileResponse
 }
