0c1d6961acfe22567006c161bde6c6f427c1a64d
MenaceHecker <thedynamic2508@gmail.com>
2026-03-10 12:30:05 -0400

    Updated incident api

--- app/src/main/java/com/example/signalops/data/remote/IncidentApi.kt
+++ app/src/main/java/com/example/signalops/data/remote/IncidentApi.kt
@@ -1,9 +1,15 @@
 package com.example.signalops.data.remote
 
+import com.example.signalops.data.remote.dto.CreateIncidentRequest
 import com.example.signalops.data.remote.dto.IncidentResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface IncidentApi {
     @GET("/api/incidents")
     suspend fun getIncidents(): List<IncidentResponse>
+
+    @POST("/api/incidents")
+    suspend fun createIncident(@Body req: CreateIncidentRequest): IncidentResponse
 }
