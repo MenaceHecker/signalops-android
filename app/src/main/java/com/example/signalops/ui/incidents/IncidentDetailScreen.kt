ab81ccf173406979ad60efab551421f4ea818c9b
MenaceHecker <thedynamic2508@gmail.com>
2026-03-09 21:55:59 -0400

    Made a ui with elavted card and text

--- app/src/main/java/com/example/signalops/ui/incidents/IncidentDetailScreen.kt
+++ app/src/main/java/com/example/signalops/ui/incidents/IncidentDetailScreen.kt
@@ -1,4 +1,54 @@
 package com.example.signalops.ui.incidents
 
-class IncidentDetailScreen {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun IncidentDetailScreen(
+    title: String,
+    severity: String,
+    status: String,
+    createdAt: String,
+    onBack: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Text(
+            text = "Incident Details",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        ElevatedCard(modifier = Modifier.fillMaxWidth()) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Spacer(Modifier.height(10.dp))
+                Text("Severity: $severity")
+                Text("Status: $status")
+                Text("Created: $createdAt")
+            }
+        }
+
+        Button(onClick = onBack) {
+            Text("Back")
+        }
+    }
 }
